c1fab0070d224782f9fb761d1a4bb1b5770f5b17
Student <example@example.com>
2019-10-15 11:11:39 -0700

    Add form

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -1,24 +1,44 @@
 using Microsoft.AspNetCore.Mvc;
 using AnimalShelter.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AnimalShelter.Controllers
 {
   public class AnimalsController : Controller
-  {     
-    [HttpGet("/animals")]
+  {
+    private readonly AnimalShelterContext _db;
+
+    public AnimalsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
     public ActionResult Index()
     {
-        List<Animal> allAnimals = Animal.GetAll();
-        return View(allAnimals);
+      List<Animal> model = _db.Animals.ToList();
+      return View(model);
     }
 
-    [HttpGet("/animals/new")]
-    public ActionResult New()
+    public ActionResult Create()
     {
-   
-      return View();
+        return View();
     }
 
+    [HttpPost]
+    public ActionResult Create(Animal animal)
+    {
+        _db.Animals.Add(animal);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+     public ActionResult Details(int id)
+    {
+        Animal thisAnimal = _db.Animals.FirstOrDefault(animals => animals.AnimalId == id);
+        //^This is our way of communicating to the database, giving it the id that was passed in and telling the database to give us the item in the database that has this id. In this line, "items" is not a special name.
+        return View(thisAnimal);
+    }
+    }
   }
-}
+
